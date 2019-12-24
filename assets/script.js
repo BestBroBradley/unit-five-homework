2e3a8fd72e851b99af770e16f4fd80f603b0bbb6
Allison Bradley <allison.bradley@outlook.com>
2019-12-23 17:16:14 -0700

    Added 0 if less than 10 mins past hour

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var timeP = $("#timeP")
 
 var dt = new Date();
 
+function pageOpen() {
+
 if (($(dt.getDay())[0]) === 1) {
     dateP.text(`Monday, ${dt.toLocaleDateString()}`);
 } else if (($(dt.getDay())[0]) === 2) {
@@ -20,60 +22,112 @@ if (($(dt.getDay())[0]) === 1) {
 }
 
 if (($(dt.getHours())[0]) === 0) {
-    timeP.text(`12:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`12:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`12:${dt.getMinutes()} AM`)}
 } else if (($(dt.getHours())[0]) === 1) {
-  timeP.text(`1:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`1:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`1:${dt.getMinutes()} AM`)}
 } else if (($(dt.getHours())[0]) === 2) {
-    timeP.text(`2:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`2:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`2:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 3) {
-    timeP.text(`3:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`3:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`3:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 4) {
-    timeP.text(`4:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`4:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`4:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 5) {
-    timeP.text(`5:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`5:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`5:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 6) {
-    timeP.text(`6:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`6:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`6:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 7) {
-    timeP.text(`7:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`7:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`7:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 8) {
-    timeP.text(`8:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`8:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`8:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 9) {
       $("#am9").addClass("currentHour")
-    timeP.text(`9:${dt.getMinutes()} AM`)
+      if (dt.getMinutes() < 10) {
+        timeP.text(`9:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`9:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 10) {
     $("#am10").addClass("currentHour")
-    timeP.text(`10:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`10:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`10:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 11) {
     $("#am11").addClass("currentHour")
-    timeP.text(`11:${dt.getMinutes()} AM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`11:0${dt.getMinutes()} AM`)
+    } else {timeP.text(`11:${dt.getMinutes()} AM`)}
   } else if (($(dt.getHours())[0]) === 12) {
     $("#pm12").addClass("currentHour")
-    timeP.text(`12:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`12:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`12:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 13) {
     $("#pm1").addClass("currentHour")
-    timeP.text(`1:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`1:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`1:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 14) {
     $("#pm2").addClass("currentHour")
-    timeP.text(`2:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`2:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`2:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 15) {
     $("#pm3").addClass("currentHour")
-    timeP.text(`3:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`3:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`3:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 16) {
     $("#pm4").addClass("currentHour")
-    timeP.text(`4:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`4:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`4:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 17) {
     $("#pm5").addClass("currentHour")
-    timeP.text(`5:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`5:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`5:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 18) {
-    timeP.text(`6:${dt.getMinutes()} PM`)
+       if (dt.getMinutes() < 10) {
+        timeP.text(`6:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`6:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 19) {
-    timeP.text(`7:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`7:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`7:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 20) {
-    timeP.text(`8:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`8:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`8:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 21) {
-    timeP.text(`9:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`9:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`9:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 22) {
-    timeP.text(`10:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`10:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`10:${dt.getMinutes()} PM`)}
   } else if (($(dt.getHours())[0]) === 23) {
-    timeP.text(`11:${dt.getMinutes()} PM`)
+    if (dt.getMinutes() < 10) {
+        timeP.text(`11:0${dt.getMinutes()} PM`)
+    } else {timeP.text(`11:${dt.getMinutes()} PM`)}
   }
+
+}
+
+pageOpen();
