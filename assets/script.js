57cebf05844ac6c2c524fce193bf829567956c49
Allison Bradley <allison.bradley@outlook.com>
2019-12-23 19:00:35 -0700

    Set text input field to save to local storage

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,9 @@ var dt = new Date();
 
 function pageOpen() {
 
-    var todoArray = [
+    var todoArray
+    
+    = [
         "Test0",
         "Test1",
         "Test2",
@@ -15,9 +17,9 @@ function pageOpen() {
         "Test6",
         "Test7",
         "Test8"
-]
+    ]
 
-// todoArray = JSON.parse(localStorage.getItem("todo Array")) || [];
+    todoArray = JSON.parse(localStorage.getItem("todo array")) || [];
 
     if (($(dt.getDay())[0]) === 1) {
         dateP.text(`Monday, ${dt.toLocaleDateString()}`);
@@ -52,150 +54,152 @@ function pageOpen() {
             timeP.text(`2:0${dt.getMinutes()} AM`)
         } else { timeP.text(`2:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 3) {
         if (dt.getMinutes() < 10) {
             timeP.text(`3:0${dt.getMinutes()} AM`)
         } else { timeP.text(`3:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 4) {
         if (dt.getMinutes() < 10) {
             timeP.text(`4:0${dt.getMinutes()} AM`)
         } else { timeP.text(`4:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 5) {
         if (dt.getMinutes() < 10) {
             timeP.text(`5:0${dt.getMinutes()} AM`)
         } else { timeP.text(`5:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 6) {
         if (dt.getMinutes() < 10) {
             timeP.text(`6:0${dt.getMinutes()} AM`)
         } else { timeP.text(`6:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 7) {
         if (dt.getMinutes() < 10) {
             timeP.text(`7:0${dt.getMinutes()} AM`)
         } else { timeP.text(`7:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 8) {
         if (dt.getMinutes() < 10) {
             timeP.text(`8:0${dt.getMinutes()} AM`)
         } else { timeP.text(`8:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 9) {
         $("#am9").addClass("currentHour")
         if (dt.getMinutes() < 10) {
             timeP.text(`9:0${dt.getMinutes()} AM`)
         } else { timeP.text(`9:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 10) {
         $("#am10").addClass("currentHour")
         if (dt.getMinutes() < 10) {
             timeP.text(`10:0${dt.getMinutes()} AM`)
         } else { timeP.text(`10:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 11) {
         $("#am11").addClass("currentHour")
         if (dt.getMinutes() < 10) {
             timeP.text(`11:0${dt.getMinutes()} AM`)
         } else { timeP.text(`11:${dt.getMinutes()} AM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 12) {
         $("#pm12").addClass("currentHour")
         if (dt.getMinutes() < 10) {
             timeP.text(`12:0${dt.getMinutes()} PM`)
         } else { timeP.text(`12:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 13) {
         $("#pm1").addClass("currentHour")
         if (dt.getMinutes() < 10) {
             timeP.text(`1:0${dt.getMinutes()} PM`)
         } else { timeP.text(`1:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 14) {
         $("#pm2").addClass("currentHour")
         if (dt.getMinutes() < 10) {
             timeP.text(`2:0${dt.getMinutes()} PM`)
         } else { timeP.text(`2:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 15) {
         $("#pm3").addClass("currentHour")
         if (dt.getMinutes() < 10) {
             timeP.text(`3:0${dt.getMinutes()} PM`)
         } else { timeP.text(`3:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 16) {
         $("#pm4").addClass("currentHour")
         if (dt.getMinutes() < 10) {
             timeP.text(`4:0${dt.getMinutes()} PM`)
         } else { timeP.text(`4:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 17) {
         $("#pm5").addClass("currentHour")
         if (dt.getMinutes() < 10) {
             timeP.text(`5:0${dt.getMinutes()} PM`)
         } else { timeP.text(`5:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 18) {
         if (dt.getMinutes() < 10) {
             timeP.text(`6:0${dt.getMinutes()} PM`)
         } else { timeP.text(`6:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 19) {
         if (dt.getMinutes() < 10) {
             timeP.text(`7:0${dt.getMinutes()} PM`)
         } else { timeP.text(`7:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 20) {
         if (dt.getMinutes() < 10) {
             timeP.text(`8:0${dt.getMinutes()} PM`)
         } else { timeP.text(`8:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 21) {
         if (dt.getMinutes() < 10) {
             timeP.text(`9:0${dt.getMinutes()} PM`)
         } else { timeP.text(`9:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 22) {
         if (dt.getMinutes() < 10) {
             timeP.text(`10:0${dt.getMinutes()} PM`)
         } else { timeP.text(`10:${dt.getMinutes()} PM`) }
     }
-    
+
     else if (($(dt.getHours())[0]) === 23) {
         if (dt.getMinutes() < 10) {
             timeP.text(`11:0${dt.getMinutes()} PM`)
         } else { timeP.text(`11:${dt.getMinutes()} PM`) }
     }
 
-    $("button").on("click", function() {
+    $("button").on("click", function () {
         var textbox = (this.parentElement.parentElement.getElementsByTagName("textarea")[0]);
         var newNotes = (textbox).value
         textbox = $(textbox)
         var i = parseInt(textbox.attr("index"))
         todoArray[i] = newNotes
+        localStorage.setItem("todo array", JSON.stringify(todoArray))
+        console.log(localStorage["todo array"])
 
-      });
+    });
 
 }
 
